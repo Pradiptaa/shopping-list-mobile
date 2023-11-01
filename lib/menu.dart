d6447225aa639aa105c8d1910c5a7d76bfd3a3f3
Pradiptaa <diptaarya18@gmail.com>
2023-11-01 16:06:27 +0700

    Finished tutorial 6

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
-    MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
-    final List<ShopItem> items = [
-    ShopItem("Lihat Produk", Icons.checklist),
-    ShopItem("Tambah Produk", Icons.add_shopping_cart),
-    ShopItem("Logout", Icons.logout),
-];
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Shopping List',
+
         ),
+        backgroundColor: Colors.indigo, 
+        foregroundColor: Colors.white,
       ),
       body: SingleChildScrollView(
         // Widget wrapper yang dapat discroll
@@ -55,16 +68,8 @@ class MyHomePage extends StatelessWidget {
         ),
       ),
     );
-    }
+  }
 }
-
-class ShopItem {
-  final String name;
-  final IconData icon;
-
-  ShopItem(this.name, this.icon);
-}
-
 
 class ShopCard extends StatelessWidget {
   final ShopItem item;
@@ -110,3 +115,16 @@ class ShopCard extends StatelessWidget {
     );
   }
 }
+
+class ShopItem {
+  final String name;
+  final IconData icon;
+
+  ShopItem(this.name, this.icon);
+}
+
+final List<ShopItem> items = [
+  ShopItem("Lihat Produk", Icons.checklist),
+  ShopItem("Tambah Produk", Icons.add_shopping_cart),
+  ShopItem("Logout", Icons.logout),
+];
